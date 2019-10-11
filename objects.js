d14a40110451e9d90f103c754643eb6d5ddb9d33
Jean-François Rompré <jrompre@gmail.com>
2019-10-11 18:08:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,8 +2,9 @@
 var playlist = {Mozart: "Symphony 40"}
 
 function updatePlaylist(list, k, v) {
-/*if (playlist.Mozart) {
-    delete playlist.Mozart;
-  }*/
   Object.assign(list, {[k]:v});
+}
+
+function removeFromPlaylist(list, k) {
+  delete list[k];
 }
